cf2809f71b296789db6309a1956a38389ab59726
eolculnamo2 <rbertram8@gmail.com>
2019-05-03 00:16:27 -0500

    add mobile menu and animated hamburger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,10 @@ function Header(props) {
 
   const [hState, changeState] = useState({mobileOpen: false});
 
-  const scrollTo = reference => new ScrollTo(reference, 2);
+  const scrollTo = reference => {
+    new ScrollTo(reference, 2)
+    changeState({mobileOpen: false})
+  };
   // window.scrollTo({ top: reference.current.offsetTop-50, behavior: 'smooth' });
 
   const toggleHamburger = () => changeState({mobileOpen: !hState.mobileOpen});
@@ -21,12 +24,13 @@ function Header(props) {
 
   return (
     <div className="header-wrap">
-      <div className="header-hamburger-wrap">
-        <div className="header-hamburger-line" />
-        <div className="header-hamburger-line" />
-        <div className="header-hamburger-line" />
+      <div className={`header-hamburger-wrap ${hState.mobileOpen ? 'header-hamburger-wrap--open':''}`} onClick={toggleHamburger}>
+        <div className="header-hamburger-line line1" />
+        <div className="header-hamburger-line line2" />
+        <div className="header-hamburger-line line3" />
       </div>
-      <ul className="header-links">
+      <ul
+        className={`header-links ${hState.mobileOpen ? 'header-links--mobile-open':''}`}>
         <li onClick={() => scrollTo(homeRef,2)}>Home</li>
         <li onClick={() => scrollTo(aboutMeRef,2)}>About</li>
         <li onClick={() => scrollTo(projectsRef,2)}>Projects</li>
